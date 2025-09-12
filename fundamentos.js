3b4c7f70c46522ccc2b7cbdfea03ed33b6c630d1
Leonidas731 <leonidas.bona@escola.pr.gov.br>
2025-09-12 14:55:00 -0300

    Create fundamentos.js

---
+++ fundamentos.js
@@ -0,0 +1,64 @@
+function calcularProduto(a, b) {
+  let resultado = a * b;
+  debugger;
+  return resultado;
+}
+
+
+let i = 5;
+
+while (i >= 1) {
+  console.log(i);
+  i--;
+}
+
+
+console.log("olá mundo!")
+
+
+let n1 = 7;
+let n2 = 8;
+let n3 = 9;
+
+// Cálculo da média
+let media = (n1 + n2 + n3) / 3;
+
+// Impressão do resultado
+console.log("A média é: " + media);
+
+
+
+for (let i = 65; i <= 90; i++) {
+    // A função String.fromCharCode converte o código ASCII para o caractere correspondente
+    console.log(String.fromCharCode(i));
+}
+
+
+// Declaração da variável num
+let num = 7; // Você pode mudar esse valor para testar com outros números
+
+// Estrutura if-else para verificar se o número é par ou ímpar
+if (num % 2 === 0) {
+    console.log("O número é par");
+} else {
+    console.log("O número é ímpar");
+}
+
+
+
+// Definição da função soma
+function soma(num1, num2) {
+    return num1 + num2;
+}
+
+// Exemplo de uso da função
+let resultado = soma(5, 10);
+console.log("A soma é: " + resultado);
+
+
+
+// Declaração e inicialização da variável idade
+let idade = 30;
+
+// Imprime o valor de idade no console
+console.log("A idade é: " + idade);
