982f1b3e14539a0b07146a01a1b341a855145286
Leonidas731 <leonidas.bona@escola.pr.gov.br>
2025-11-12 15:03:32 -0300

    Create CalculaPreco.py

---
+++ CalculaPreco.py
@@ -0,0 +1,8 @@
+# Define função para calcular preço final
+def calcular_preco_final(nome, preco, desconto=0):
+    print(f"Preço inicial de {nome}: R$ {preco:.2f}")
+    preco_final = preco - (preco * desconto / 100)
+    print(f"Preço final de {nome}: R$ {preco_final:.2f}")
+
+# Exemplo de uso
+calcular_preco_final("Fone de ouvido", 200, 25)
